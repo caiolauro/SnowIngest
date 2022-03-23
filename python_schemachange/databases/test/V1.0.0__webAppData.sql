775bd138b8f878ad81b9eb8d3e06e960e9401eb4
Caio Lauro <clauro@galileo-ft.com>
2022-03-22 22:28:12 -0300

    update schemachange files

--- python_schemachange/databases/test/V1.0.0__webAppData.sql
+++ python_schemachange/databases/test/V1.0.0__webAppData.sql
@@ -1,7 +1,7 @@
-CREATE TABLE SNOWFLAKE_TECH_TALK_TEST_2.TEST.WEB_APP 
+CREATE OR REPLACE TABLE SNOWFLAKE_TECH_TALK_TEST_2.TEST.WEB_APP 
 (
   product_name varchar(100),
   category varchar(100),
   price number(18,2),
   ingestion_time timestamp 
-)
+);
