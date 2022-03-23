d28e91bed59b07f18fc16815fe041221a88a7adf
Caio Lauro <clauro@galileo-ft.com>
2022-03-22 21:12:02 -0300

    tech-talk-2 presentation trial

--- terraform_infra/snowflake/stages/tech_talk_stage.tf
+++ terraform_infra/snowflake/stages/tech_talk_stage.tf
@@ -1,15 +1,15 @@
 resource "snowflake_stage" "tech_talk_stage" {
-  name        = "TECH_TALK_STAGE"
-  url         = "s3://snowflake-tech-talk-stage"
-  database    = "SHARED"
-  schema      = "PUBLIC"
-  storage_integration = "TECH_TALK_STORAGE_INTEGRATION"
+  name        = "TECH_TALK_TEST_STAGE_2"
+  url         = "s3://web-app-tech-talk-stage-2"
+  database    = "SNOWFLAKE_TECH_TALK_TEST_2"
+  schema      = "TEST"
+  storage_integration = "TECH_TALK_STORAGE_INTEGRATION_2"
 }
 
-# resource "snowflake_stage_grant" "grant_tech_talk_stage" {
-#   database_name = snowflake_stage.tech_talk_stage.database
-#   schema_name   = snowflake_stage.tech_talk_stage.schema
-#   roles         = ["SYSADMIN"]
-#   privilege     = "OWNERSHIP"
-#   stage_name    = snowflake_stage.tech_talk_stage.name
-# }
+resource "snowflake_stage_grant" "grant_usage_tech_talk_stage" {
+  database_name = snowflake_stage.tech_talk_stage.database
+  schema_name   = snowflake_stage.tech_talk_stage.schema
+  roles         = ["SYSADMIN"]
+  privilege     = "USAGE"
+  stage_name    = snowflake_stage.tech_talk_stage.name
+}
